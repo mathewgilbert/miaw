b8ca4878587eddca7d89bbadae77b8eafd3deaf0
Mathew <matwewgilbert@gmail.com>
2025-12-27 15:48:13 +0000

    miaw package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { Elysia, t } from 'elysia'
+import { Elysia, t } from "elysia";
 
-export default new Elysia() 
+const app = new Elysia() 
     .get('/', () => 'Hello Vercel Function')
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String()
         })
-    })
+      })
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
